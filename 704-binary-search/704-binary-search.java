25500d760cfead95d82e7639d8a5fe60914aead8
BriksinDima <briksindima@gmail.com>
2022-04-04 09:10:49 +0300

    Time: 0 ms (100.00%), Space: 42.8 MB (89.29%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -6,13 +6,13 @@ class Solution {
             int mid = left + (right - left)/2;
             if(nums[mid] == target){
                 return mid;
-            } else if (nums[mid] < target){
+            } else if(nums[mid] < target){
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return - 1;
+        return  -1;
     
     }
 
