7de4941404b4638560431d654c39b5846cfc7f15
BriksinDima <briksindima@gmail.com>
2022-04-05 20:01:39 +0300

    Time: 1 ms (47.88%), Space: 41.8 MB (55.84%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,10 +1,9 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        char[] chars = s.toCharArray();
         int balance = 0;
         int result = 0;
-        for(int i = 0; i < chars.length; i++){
-            if(chars[i] == 'L'){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'L'){
                 balance++;
             } else {
                 balance--;
