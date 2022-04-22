103ec9ad346cf13c1ee323133a91f5dfccdd1518
BriksinDima <briksindima@gmail.com>
2022-04-22 22:35:36 +0300

    Time: 1 ms (80.20%), Space: 65.6 MB (6.01%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int inputK = k;
-
-
-        int[] tempArr = new int[nums.length];
+       int inputK = k;
+       int[] tempArr = new int[nums.length];
        int correctK =  inputK % nums.length; //  31 % 7 = 3
-        int shiftedOnKFromEnd = nums.length - correctK;
+       int shiftedOnKFromEnd = nums.length - correctK;
        int j = 0;
 
        // [5, 6, 7]
