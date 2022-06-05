035c90e121bdb465070da8eaa1dc3e1148094c2f
BriksinDima <briksindima@gmail.com>
2022-06-05 22:26:37 +0300

    Time: 0 ms (100.00%), Space: 44.3 MB (21.74%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,15 +14,19 @@
  * }
  */
 class Solution {
+public boolean validUtil(TreeNode node, long low, long high) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= low || node.val >= high) {
+            return false;
+        }
+        return validUtil(node.left, low, node.val) && validUtil(node.right, node.val, high);//upper bound of left subtree is node.val same is lower bound for right sub tree
+    }
 
-    
-   public boolean validUtil(TreeNode node,long low,long high){
-        if(node==null){return true;}
-        if(node.val<=low || node.val>=high){return false;}
-        return validUtil(node.left,low,node.val)&&validUtil(node.right,node.val,high);//upper bound of left subtree is node.val same is lower bound for right sub tree
-    }
-    
     public boolean isValidBST(TreeNode root) {
-        return validUtil(root,Long.MIN_VALUE,Long.MAX_VALUE);//root node value range
+        return validUtil(root, Long.MIN_VALUE, Long.MAX_VALUE);//root node value range
     }
+    
+ 
 }
