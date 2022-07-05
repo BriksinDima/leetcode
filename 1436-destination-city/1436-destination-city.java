66ddfe5adc6ef2bd73307300a640d02776b43c01
BriksinDima <briksindima@gmail.com>
2022-07-05 23:21:34 +0300

    Time: 2 ms (96.63%), Space: 43.8 MB (55.27%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+     if(paths.size() == 1)
+            return paths.get(0).get(1);
+            
+        Map<String, String> map = new HashMap<>();
+        for(List<String> path: paths){
+            map.put(path.get(0), path.get(1));
+        }
+        
+        String destination = paths.get(0).get(0);
+        while(map.containsKey(destination)){
+            destination = map.get(destination);
+        }
+        
+        return destination;   
+    }
+}
